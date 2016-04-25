87cf786249d7bb3bec1a378571a928f91f6d9df0
Kieran Butler <kieranbutler22@gmail.com>
2016-04-25 16:46:08 +0100

    Added new paragraph templates

--- templates/paragraphs-item--side-by-side-image-and-text.tpl.php
+++ templates/paragraphs-item--side-by-side-image-and-text.tpl.php
@@ -7,7 +7,7 @@
 // Get the column classes.
 $image_cols = intval($content['field_parapg_image_width']['#items'][0]['value']);
 $image_class = 'col-md-' . $image_cols;
-$text_class = 'col-md-' . (12 - $image_cols)
+$text_class = 'col-md-' . (12 - $image_cols);
 
 ?>
 
@@ -28,9 +28,14 @@ $text_class = 'col-md-' . (12 - $image_cols)
 
       <?php
       // Are we showing image on the right?
-      if ($content['field_parapg_image_side']['#items'][0]['value'] == 'right'): ?>
+      if ($content['field_parapg_image_side']['#items'][0]['value'] == 'right'
+          && empty($content['field_parapg_youtube_video_url'])): ?>
         <div class="image <?php print $image_class ?>">
           <?php print render($content['field_parapg_image']) ?>
+        </div>
+      <?php else: ?>
+        <div class="video <?php print $image_class ?>">
+          <?php print render($content['field_parapg_youtube_video_url']);?>
         </div>
       <?php endif ?>
     </div>
