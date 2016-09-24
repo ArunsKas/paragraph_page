77b4f45fd2965f84fccfadea6c0bad05137aec42
Alex Harries <alex@greyhead.co.uk>
2016-09-24 02:24:16 +0200

    Re-jig images paragraphs.
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- templates/paragraphs-item--images.tpl.php
+++ templates/paragraphs-item--images.tpl.php
@@ -7,20 +7,26 @@
 // Set some variaboos for ease of programming.
 $caption = '';
 if (array_key_exists('field_paragraphs_caption', $content)) {
-  $caption = $content['field_paragraphs_caption'][0]['#markup'];
+  $caption = render($content['field_paragraphs_caption']);
 }
 ?>
 
 <div class="paragraphs-item paragraphs-item--images">
   <div class="row">
     <div class="container">
-      <div class="col-md-12">
-        <?php print render($content['field_paragraph_images']) ?>
+      <?php //print render($content['field_paragraph_images']) ?>
 
-        <?php if (!empty($caption)): ?>
-          <div class="paragraphs-item paragraphs-item--images-caption"><?php print $caption ?></div>
-        <?php endif ?>
-      </div>
+      <?php foreach (element_children($content['field_paragraph_images']) as $field_paragraph_image) { ?>
+        <div class="col-md-3 paragraphs-item--image">
+          <?php print render($content['field_paragraph_images'][$field_paragraph_image]) ?>
+        </div>
+      <?php } ?>
+
+      <?php if (!empty($caption)): ?>
+        <div class="col-md-12 paragraphs-item--images-caption">
+          <?php print $caption ?>
+        </div>
+      <?php endif ?>
     </div>
   </div>
 </div>
